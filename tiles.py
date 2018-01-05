f0e91eabb898d2450b7a8e4069b555f21d0390d6
karina <karinayeznaian@yahoo.com>
2018-01-04 23:17:11 -0800

    part 4 done

--- tiles.py
+++ tiles.py
@@ -1,9 +1,28 @@
-import items, enemies
+import items, enemies, actions, world
 
 class MapTile:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
+
+	def adjacent_moves(self):
+	        """Returns all move actions for adjacent tiles."""
+	        moves = []
+	        if world.tile_exists(self.x + 1, self.y):
+	                moves.append(actions.MoveEast())
+	        if world.tile_exists(self.x - 1, self.y):
+	                moves.append(actions.MoveWest())
+	        if world.tile_exists(self.x, self.y - 1):
+	                moves.append(actions.MoveNorth())
+	        if world.tile_exists(self.x, self.y + 1):
+	                moves.append(actions.MoveSouth())
+	        return moves
+
+	def available_actions(self):
+	        """Returns all of the available actions in this room."""
+	        moves = self.adjacent_moves()
+	        moves.append(actions.ViewInventory())
+	        return moves
 
 
 #shit's going downnnnn
@@ -53,6 +72,12 @@ class EnemyRoom(MapTile):
 				print("{} does {} damage. You have {} HP remaining.".format(self.enemy.name, self.enemy.damage, the_player.hp))
 			else:
 				print("{} does {} damage. You have {} HP remaining. lol u got fuuuuuucked up.".format(self.enemy.name, self.enemy.damage, the_player.hp))
+
+	def available_actions(self):
+		if self.enemy.is_alive():
+			return [actions.Flee(tile=self), actions.Attack(enemy=self.enemy)]
+		else:
+			return self.adjacent_moves()
 
 
 #plain rooms
